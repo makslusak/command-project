eed8cd380406bd7445f9e9acfa1ec7ee0eb79c0e
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-07 16:04:16 +0300

    add new modal windows in section about

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-about-open]'),
-    closeModalBtn: document.querySelector('[data-about-close]'),
-    modal: document.querySelector('[data-about]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-about-open]'),
+//     closeModalBtn: document.querySelector('[data-about-close]'),
+//     modal: document.querySelector('[data-about]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('myBtn');
+var span = document.getElementsByClassName('about-us__button')[0];
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
   }
-})();
+};
