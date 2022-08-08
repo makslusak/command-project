d1f1c95bc48bf0ce04dde4acb8185fc80fe049f8
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-08-08 09:48:29 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    menuList: document.querySelector('.header__list'),
   };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.menuList.addEventListener('click', removeModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+  }
+  function removeModal() {
+    refs.modal.classList.add('is-hidden');
   }
 })();
